0157b50562da3639167ef3854b8f02defb58d4a9
anuksks <my.browser@rediffmail.com>
2016-06-24 15:06:29 +0530

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,1 +1,15 @@
-<?php echo "hello world" ?>
+<DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>my php</title>
+        <meta name="description" content="anu">
+        <meta http-equiv="refresh" content="5">
+        <meta name="author" content="anuks">
+        <meta name="application-name" content="my php"><meta name="keywords" content="anu, app, kop"><meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="anu.css">
+    </head>
+    <body>
+        <?php echo "hello my php"?>
+    </body>
+    </html>
